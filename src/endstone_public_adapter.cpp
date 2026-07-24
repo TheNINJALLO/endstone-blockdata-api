7f572dc32bc4ef88c68e79fea9536abb93c71dca
TheN1NJ4LL0 <It@floodman.com>
2026-07-24 10:23:56 -0400

    fix(cmake): patch Endstone identifier.h format_context::iterator typo for Clang 18

--- src/endstone_public_adapter.cpp
+++ src/endstone_public_adapter.cpp
@@ -1,13 +1,3 @@
-#include <format>
-namespace std {
-#if defined(_LIBCPP_VERSION)
-inline namespace __1 {
-typedef ::std::format_context format_context;
-}
-#endif
-typedef ::std::format_context format_context;
-}
-
 #include "endstone_blockdata/endstone_adapter.h"
 #include <endstone/endstone.hpp>
 #include <exception>
